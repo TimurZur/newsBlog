7f4474962e70db40ebb8ac5f30a904bc6f1f90db
TimurZur <zuraev.timur2016@yandex.ru>
2020-07-25 23:15:11 +1100

    changed buttonCreate look

--- resources/views/elements/header.blade.php
+++ resources/views/elements/header.blade.php
@@ -2,6 +2,6 @@
     <h5 class="my-0 mr-md-auto font-weight-normal">NewsBlog</h5>
     <nav class="my-2 my-md-0 mr-md-3">
         <a class="p-2 text-dark" href="{{route('home')}}">Главная</a>
-        <a class="p-2 text-dark" href="{{route('create')}}">Создать</a>
+        <a class="btn btn-outline-primary" href="{{route('create')}}">Создать</a>
     </nav>
 </div>
